0afeaa393b1149fbca1a84fc9279909051bab801
tombatron <tommy.hanks@gmail.com>
2021-02-13 08:54:08 -0500

    Cleaned up some unused imports.

--- src/native/JavaScript_Eval_Native/src/lib.rs
+++ src/native/JavaScript_Eval_Native/src/lib.rs
@@ -1,8 +1,5 @@
+use std::ffi::{CStr, CString};
 use std::os::raw::c_char;
-use std::{
-    convert::TryInto,
-    ffi::{CStr, CString},
-};
 
 use function_parameter::FunctionParameter;
 use primitive_result::PrimitiveResult;

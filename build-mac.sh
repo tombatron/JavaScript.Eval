171da138efac17de8003e86e1fa2ebe1907b0fa3
Tombatron <tommy.hanks@gmail.com>
2021-04-18 08:13:51 -0400

    Let's try with lib prefixed?

--- build-mac.sh
+++ build-mac.sh
@@ -1,2 +1,2 @@
 cargo build --release --manifest-path src/native/JavaScript_Eval_Native/Cargo.toml --target-dir /tmp
-cp /tmp/release/javascript_eval_native.dylib libs/runtimes/osx-x64/native/.
+cp /tmp/release/libjavascript_eval_native.dylib libs/runtimes/osx-x64/native/.

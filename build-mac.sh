62054e931424a3f487a721fa4b5bfd908237cb8f
Tombatron <tommy.hanks@gmail.com>
2021-04-18 08:38:20 -0400

    Create that directory first...

--- build-mac.sh
+++ build-mac.sh
@@ -1,2 +1,3 @@
 cargo build --release --manifest-path src/native/JavaScript_Eval_Native/Cargo.toml --target-dir /tmp
+mkdir libs/runtimes/osx-x64/native
 cp /tmp/release/libjavascript_eval_native.dylib libs/runtimes/osx-x64/native/.
